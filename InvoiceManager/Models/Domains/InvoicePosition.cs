07c1508891d3f55264d1337437841f17a1a0339d
tomwiet <tthanquol@gmail.com>
2024-01-03 19:07:54 +0100

    temp

--- InvoiceManager/Models/Domains/InvoicePosition.cs
+++ InvoiceManager/Models/Domains/InvoicePosition.cs
@@ -11,10 +11,11 @@ namespace InvoiceManager.Models.Domains
         public int Id { get; set; }
         public int Lp { get; set; }
         public int InvoiceId { get; set; }
-        public decimal Value {  get; set; }
+        public decimal Value { get; set; }
         public int ProductId { get; set; }
         public int Quantity { get; set; }
 
         public Invoice Invoice { get; set; }
         public Product Product { get; set; }
+    }
 }
