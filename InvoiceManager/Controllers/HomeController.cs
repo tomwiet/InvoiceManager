b1a4f1c4679132dc42ec0752e93238df388a5074
tomwiet <tthanquol@gmail.com>
2024-01-05 21:33:30 +0100

    Added storing invoice data in db (add edit) Added storing invoice positions data in db (add edit)

--- InvoiceManager/Controllers/HomeController.cs
+++ InvoiceManager/Controllers/HomeController.cs
@@ -14,6 +14,8 @@ namespace InvoiceManager.Controllers
     public class HomeController : Controller
     {
         private InvoiceRepository _invoiceRepository = new InvoiceRepository();
+        private ClientRepository _clientRepository = new ClientRepository();
+        private ProductRepository _productRepository = new ProductRepository();
         public ActionResult Index()
         {
             var userId = User.Identity.GetUserId();
@@ -38,12 +40,12 @@ namespace InvoiceManager.Controllers
             return new EditInvoiceViewModel
             {
                 Invoice = invoice,
-                Heading = invoice.Id == 0 ? "Dodawanie nowej faktury": "Faktura",
+                Heading = invoice.Id == 0 ? "Dodawanie nowej faktury" : "Faktura",
                 Clients = _clientRepository.GetClients(userId),
                 MethodOfPayments = _invoiceRepository.GetMethodsOfPayment()
 
 
-            }
+            };
         }
 
         private Invoice GetNewInvoice(string userId)
@@ -61,39 +63,107 @@ namespace InvoiceManager.Controllers
             int invoiceId,
             int invoicePositionId=0) 
         {
-            EditInvoicePositionViewModel vm = null;
-            if (invoicePositionId == 0) 
+            var userId = User.Identity.GetUserId();
+            var invoicePosition = invoicePositionId==0 ? 
+                GetNewPosition(invoicePositionId,invoiceId) :
+                _invoiceRepository.GetInvoicePosition(invoicePositionId,userId);
+            var vm = PrepareInvoicePositionVm(invoicePosition);
+            return View(vm); 
+        }
+
+        private EditInvoicePositionViewModel PrepareInvoicePositionVm(InvoicePosition invoicePosition)
+        {
+            return new EditInvoicePositionViewModel 
+            { 
+                InvoicePosition = invoicePosition,
+                Heading= invoicePosition.Id==0 ? 
+                "Dodawanie pozycji faktury" : "Pozycja faktury",
+                Products = _productRepository.GetProducts()
+            };
+        }
+
+        private InvoicePosition GetNewPosition(int invoicePositionId, int invoiceId)
+        {
+            return new InvoicePosition
             {
-                vm = new EditInvoicePositionViewModel
-                {
-                    InvoicePosition = new InvoicePosition(),
-                    Heading = "Dodawanie nowej pozycji",
-                    Products = new List<Product>
-                    {
-                        new Product
-                        {
-                            Id = 1,
-                            Name = "Produkt 1"
-                        }
-                    }
-                };
+                InvoiceId = invoiceId,
+                Id = invoicePositionId
+            };
+        }
+        [HttpPost]
+        public ActionResult Invoice(Invoice invoice)
+        {
+            var userId = User.Identity.GetUserId();
+            invoice.UserId = userId;
+
+            if(invoice.Id==0)
+                _invoiceRepository.Add(invoice);
+            else
+                _invoiceRepository.Update(invoice);
+
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public ActionResult InvoicePosition(InvoicePosition invoicePosition)
+        {
+            var userId = User.Identity.GetUserId();
+
+            var product = _productRepository.GetProduct(invoicePosition.ProductId);
+
+            invoicePosition.Value = invoicePosition.Quantity * product.Value;
+
+            if (invoicePosition.Id == 0)
+            {
+                _invoiceRepository.AddPosition(invoicePosition, userId);
             }
             else
             {
-                vm = new EditInvoicePositionViewModel
-                {
-                    InvoicePosition = new InvoicePosition
-                    {
-                        Lp=1,Value=123,Quantity=2,ProductId=1
-                    },
-                    Heading = "Dodawanie nowej pozycji",
-                    Products = new List<Product>
-                    {
-                        new Product {Id = 1,Name = "Produkt 1"}
-                    }
-                };
+                _invoiceRepository.UpdatePosition(invoicePosition, userId);
             }
-            return View(vm); 
+
+            _invoiceRepository.UpdateInvoiceValue(invoicePosition.InvoiceId,userId);
+            
+            return RedirectToAction("Invoice",new {id=invoicePosition.InvoiceId});
+            
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var userId = User.Identity.GetUserId();
+                _invoiceRepository.Delete(id, userId);
+
+            }
+            catch (Exception exeption)
+            {
+                //logowanie błędu do pliku
+                return Json(new { Success = false, Message = exeption.Message });
+            }
+ 
+            return Json(new { Success = true });
+        }
+        [HttpPost]
+        public ActionResult DeletePosition(int id, int invoiceId)
+        {
+            var invoiceValue = 0m;
+            try
+            {
+                var userId = User.Identity.GetUserId();
+                _invoiceRepository.DeletePosition(id, userId);
+
+                invoiceValue = _invoiceRepository.UpdateInvoiceValue(invoiceId, userId);
+
+
+
+            }
+            catch (Exception exeption)
+            {
+                //logowanie błędu do pliku
+                return Json(new { Success = false, Message = exeption.Message });
+            }
+
+            return Json(new { Success = true, InvoiceValue = invoiceValue });
         }
 
         [AllowAnonymous]
