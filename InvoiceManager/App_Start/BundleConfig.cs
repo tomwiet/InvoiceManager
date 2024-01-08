b6e3831e390052cfaf2a257b79e3af4f415fc06c
tomwiet <tthanquol@gmail.com>
2024-01-08 20:31:33 +0100

    Added cookie policy info

--- InvoiceManager/App_Start/BundleConfig.cs
+++ InvoiceManager/App_Start/BundleConfig.cs
@@ -10,7 +10,8 @@ namespace InvoiceManager
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.cookie.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
