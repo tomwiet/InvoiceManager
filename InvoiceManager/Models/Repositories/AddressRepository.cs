5b63a414e860964e831e872ae50ebe406bcf6b57
Tomasz Wieteska <tomasz.wieteska@elektrykplock.edu.pl>
2024-01-12 11:42:52 +0100

    Clients adding in progress

---
+++ InvoiceManager/Models/Repositories/AddressRepository.cs
@@ -0,0 +1,24 @@
+﻿using InvoiceManager.Models.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InvoiceManager.Models.Repositories
+{
+    public class AddressRepository
+    {
+        public List<Address> GetAdresses()
+        {
+                using (var context = new ApplicationDbContext())
+                {
+                    return context.Addresses.ToList();
+                }
+            
+        }
+
+
+        
+    }
+}
---
+++ InvoiceManager/Models/ViewModels/AddEditClientViewModel.cs
@@ -0,0 +1,17 @@
+﻿using InvoiceManager.Models.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InvoiceManager.Models.ViewModels
+{
+    public class AddEditClientViewModel
+    {
+        public Client Client { get; set; }
+        public Address Address { get; set;}
+        public string Heading { get; set; }
+
+        public List<Address> Addresses { get; set; }
+    }
+}
